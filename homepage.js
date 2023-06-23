006fd394ea18325d81d5580824e3a0c610734888
Sophia Guedes <2021321844@ifam.edu.br>
2023-06-23 15:15:20 -0400

    Homepage: carrossel

--- homepage.js
+++ homepage.js
@@ -1,3 +1,22 @@
+/*carrossel principal / inicial*/
+
+const carross = document.getElementById("img");
+const img = document.querySelectorAll("#img img");
+
+let idx = 0;
+
+function carrossel(){
+    idx++;
+
+    if(idx > img.length - 1){
+        idx=0;
+    }
+    carross.style.transform = "translateX(${-idx * 500}px)";
+}
+setInterval(carrossel, 1800);
+
+
+
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav_links");
 
